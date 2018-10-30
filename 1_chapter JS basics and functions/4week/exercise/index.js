66c7efa6bedfee4fea118f5917bcaf9d6c629b04
Sevastyan Luchkin <37866548+SevastyanL@users.noreply.github.com>
2018-10-30 16:25:05 +0300

    filter collection

--- 1_chapter JS basics and functions/4week/exercise/index.js
+++ 1_chapter JS basics and functions/4week/exercise/index.js
@@ -5,17 +5,17 @@
  */
 
 function query(collection) {
-  var friends = JSON.parse(JSON.stringify(arguments[0]));
-  
+  var friends = JSON.parse(JSON.stringify(arguments[0])); //создаем копию объекта
+  //сохраняем в args функции и создаем массивы, в которые мы будем их фильтровать
   var args = [].slice.call(arguments, 1);
   var filter = [];
   var select = [];
-  
+  //отфильтровываем функции по ранее созданным массивам
   sortOperations(args, filter, select); 
-
+  //фильтруем только если есть валидные параметры
   if (filter.length > 0)
     uniqFilter(filter, friends);
-  
+  //тоже самое для select
   if (select.length > 0)
     uniqSelect(select, friends);
 
@@ -26,6 +26,7 @@ function query(collection) {
  * @params {String[]}
  */
 function select() {
+  //добавляем select для последующей фильтрации
   fit = [].slice.call(arguments);
   fit.unshift('select');
   return fit;
@@ -36,10 +37,12 @@ function select() {
  * @param {Array} values – Массив разрешённых значений
  */
 function filterIn(property, values) {
+  //добавляем filterIn для последующей фильтрации
   return ['filterIn', property, values];
 }
 
 function sortOperations(arr, filter, select) {
+  //сортируем в зависимости от операции, не должно быть undefined и пустых значений
   for (var i = 0; i < arr.length; i++) {
     if (arr[i][0] === 'filterIn' && arr[i][1] !== undefined && arr[i][2] !== undefined && arr[i][2].length !== 0) {
       filter.push(arr[i].slice(1));
@@ -51,6 +54,7 @@ function sortOperations(arr, filter, select) {
 }
 
 function uniqFilter(filter, obj) {
+  //удаляем ненужные значения
   for (var i = 0; i < obj.length; i++) {
     for (var j = 0; j < filter.length; j++) {
       if (obj[i] !== undefined && filter[j][1].indexOf(obj[i][filter[j][0]]) === -1) {
@@ -62,7 +66,7 @@ function uniqFilter(filter, obj) {
 }
 
 function uniqSelect(select, obj) {
-
+  //удаляем ненужные значения
   var keys = Object.keys(obj[0]);
 
   for (var i = 0; i < obj.length; i++) {
